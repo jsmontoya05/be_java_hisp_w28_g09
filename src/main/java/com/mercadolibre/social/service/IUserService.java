b01ccf2ff628347b7e07ab18e4e747e8d21c9915
Karina Sierra <kkarinasierrav@gmail.com>
2024-12-13 13:46:32 -0500

    Ejercicio 01 sin probar

--- src/main/java/com/mercadolibre/social/service/IUserService.java
+++ src/main/java/com/mercadolibre/social/service/IUserService.java
@@ -1,4 +1,7 @@
 package com.mercadolibre.social.service;
 
+import com.mercadolibre.social.dto.response.FollowUserResponseDto;
+
 public interface IUserService {
+    FollowUserResponseDto followUser(int userId, int userIdToFollow);
 }
