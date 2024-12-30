fa46ce7e0169825509f60822df87e21904f0fad0
redflox <swsbmm@gmail.com>
2024-12-30 16:53:13 -0500

    Se elimina dependencias que no se usan

--- src/main/java/com/mercadolibre/social/service/IProductService.java
+++ src/main/java/com/mercadolibre/social/service/IProductService.java
@@ -1,9 +1,6 @@
 package com.mercadolibre.social.service;
 
-import com.mercadolibre.social.dto.ProductSearchDto;
 import com.mercadolibre.social.dto.response.PostDetailsDTO;
-import com.mercadolibre.social.dto.response.ProductCountPromoPostDto;
-
 import java.util.List;
 
 public interface IProductService {
