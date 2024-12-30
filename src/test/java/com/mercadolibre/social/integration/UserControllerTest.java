0ba34a0c4a82e2977fb421f032f5a8681b170c81
redflox <swsbmm@gmail.com>
2024-12-30 16:53:57 -0500

    Test carga de archivo json segun contexto

--- src/test/java/com/mercadolibre/social/integration/UserControllerTest.java
+++ src/test/java/com/mercadolibre/social/integration/UserControllerTest.java
@@ -1,12 +1,19 @@
 package com.mercadolibre.social.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.social.dto.response.UserCountFollowersDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,4 +22,22 @@ class UserControllerTest {
     MockMvc mockMvc;
 
     ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("Carga de JSON segun el contexto.")
+    public void pruebaTest() throws Exception {
+        //ARRANGE
+        UserCountFollowersDto bodyEsperadoDTO = new UserCountFollowersDto("1", "john_doe_test", 0);
+        Integer parametroEntrada = 1;
+        ResultMatcher statusEsperado = status().isOk();
+        ResultMatcher contentTypeEsperado = content().contentType("application/json");
+        ResultMatcher bodyEsperado = content().json(objectMapper.writeValueAsString(bodyEsperadoDTO));
+
+        // ACT & ASSERT
+        mockMvc.perform(get("/users/{parametroEntrada}/followers/count", parametroEntrada))
+                .andExpect(statusEsperado)
+                .andExpect(contentTypeEsperado)
+                .andExpect(bodyEsperado)
+                .andDo(print());
+    }
 }
