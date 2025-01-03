7e49aa3690a907b25c4bb02ec6adcaddd6a12385
redflox <swsbmm@gmail.com>
2025-01-03 08:18:30 -0500

    Instanciando el objectMapper

--- src/test/java/com/mercadolibre/social/util/Utils.java
+++ src/test/java/com/mercadolibre/social/util/Utils.java
@@ -3,10 +3,14 @@ package com.mercadolibre.social.util;
 import java.time.LocalDate;
 import java.util.Random;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Utils {
-    public static LocalDate generatedDateOfLastMont() {
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static LocalDate generatedDateOfLastTwoWeeks() {
         LocalDate date = LocalDate.now();
-        int random = new Random().nextInt(30);
+        int random = new Random().nextInt(13);
         return date.minusDays(random);
     }
 }
